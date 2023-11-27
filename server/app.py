b80fd777a413b65934cabd73b709c06406965f14
Newton-Bundi <newton.bundi@student.moringaschool.com>
2023-11-27 20:23:45 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -7,12 +7,13 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+
 
 api = Api(app)
 
@@ -46,6 +47,8 @@ class PlantByID(Resource):
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+with app.app_context():
+    db.create_all()
--- server/models.py
+++ server/models.py
@@ -4,7 +4,7 @@ from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
+    __tablename__ = 'app'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -13,3 +13,4 @@ class Plant(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'<Plant {self.name} | Price: {self.price}>'
+    
